da66d30d898e4acc87862d7d9e54d691a62fce3e
TungJunMing <kelleymartin872@gmail.com>
2021-06-13 17:38:23 +0530

    BugFixes

---
+++ Client/src/apiServices/CouponService.js
@@ -0,0 +1,24 @@
+
+import Constants from '../DTOs/Constants';
+import ApiService from './ApiService';
+
+export default class CustomerService
+{
+    constructor()
+    {
+        this.url = Constants.APIUrl.base + Constants.APIUrl.couponService;
+    }
+
+    addCouponTxn(reqObj={})
+    {
+        /* Request structure
+        reqObj = {
+            "couponNmbr": "12341234"
+        };
+        */
+
+        const reqUrl = this.url + "addCouponTxn";
+        let apiService = new ApiService("POST", reqUrl , reqObj);
+        return apiService.getResponse();
+    }
+}
---
+++ Client/src/apiServices/ItemService.js
@@ -0,0 +1,70 @@
+
+import Constants from '../DTOs/Constants';
+import ApiService from './ApiService';
+
+export default class ItemService
+{
+    constructor()
+    {
+        this.url = Constants.APIUrl.base + Constants.APIUrl.itemService;
+    }
+
+
+    getItems(reqObj={})
+    {
+        /* Request structure
+        reqObj = {
+             "itemId": "111003",
+             "itemName": ""
+        };
+        */
+
+        const reqUrl = this.url + "getItems";
+        let apiService = new ApiService("POST", reqUrl , reqObj);
+        return apiService.getResponse();
+    }
+
+    addItemTxn(reqObj={})
+    {
+        /* Request structure
+        reqObj = {
+            "itemId": "111002",
+            "itemName": "",
+            "itemQty": 2
+        };
+        */
+
+        const reqUrl = this.url + "addItemTxn";
+        let apiService = new ApiService("POST", reqUrl , reqObj );
+        return apiService.getResponse();
+    }
+
+    setItemQty(reqObj={})
+    {
+        /* Request structure
+        reqObj = {
+            "lineNumber": "3",
+            "itemQty": 2
+        };
+        */
+
+        const reqUrl = this.url + "setItemQty";
+        let apiService = new ApiService("POST", reqUrl , reqObj );
+        return apiService.getResponse();
+    }
+
+    lineDiscount(reqObj={})
+    {
+        /* Request structure
+        reqObj = {
+            "lineNumber": "3",
+            "discountAmt": 20.5
+        };
+        */
+
+        const reqUrl = this.url + "lineDiscount";
+        let apiService = new ApiService("POST", reqUrl , reqObj );
+        return apiService.getResponse();
+    }
+    
+}
